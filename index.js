f8447c87bbe50ec089049a41bacc1e9e39a528bd
Abusad Ahmad <ahmadabusad14@gmail.com>
2025-01-05 14:32:05 +0530

    First comment

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send(`<h1>This is home page</h1>
+    <button onclick='alert("clicked")''>About</button>`);
+});
+app.listen(PORT, console.log("Server is live on", PORT));
