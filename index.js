0dbd314353159c5c78ea2bb86da5b579f23d1ff7
Abusad Ahmad <ahmadabusad14@gmail.com>
2025-01-07 21:54:23 +0530

    7 Jan

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.setHeader("Content-Type", "text/html"); // Explicitly set the Content-Type
-  res.send(`<h1>This is home page</h1>
-    <button onclick="alert('clicked')">About</button>`);
+  res.send(`<h1>This is home page</h1><h2>This is added on 7 January</h>`);
 });
 app.listen(PORT, console.log("Server is live on", PORT));
