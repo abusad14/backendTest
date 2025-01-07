961c617090077849c46e701cd30f1ba6a3b9883e
Abusad Ahmad <ahmadabusad14@gmail.com>
2025-01-07 22:00:39 +0530

    about page is added

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.setHeader("Content-Type", "text/html"); // Explicitly set the Content-Type
-  res.send(`<h1>This is home page</h1><h2>This is added on 7 January</h>`);
+  res.send(
+    `<h1>This is home page</h1><h2>This is added on 7 January</h><a href="/about">About</a>`
+  );
+});
+app.get("/about", (req, res) => {
+  res.setHeader("Content-Type", "text/html"); // Explicitly set the Content-Type
+  res.send(
+    `<h1>Welcome to About page</h1><h2>This is also added on 7 January</h><a href="/">Home</a>`
+  );
 });
 app.listen(PORT, console.log("Server is live on", PORT));
